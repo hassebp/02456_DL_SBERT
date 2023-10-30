767181f1aaa340c24d0f3e95b3392f86e516a881
hassebp <49819884+hassebp@users.noreply.github.com>
2023-10-30 21:02:11 +0100

    Setting up logging and time keeping test

--- models.py
+++ models.py
@@ -1,13 +1,21 @@
 from sentence_transformers import SentenceTransformer
+from pelutils import TT 
 
 def SBERT():
+    
+    TT.profile("Loading model")
     model = SentenceTransformer('all-MiniLM-L6-v2')
-
+    TT.end_profile()
     sentences = ['This framework generates embeddings for each input sentence',
     'Sentences are passed as a list of string.', 
     'The quick brown fox jumps over the lazy dog.']
+    TT.profile("Encoding sentences")
     sentence_embeddings = model.encode(sentences)
+    TT.end_profile()
+    TT.profile("Printing embeddings")
     for sentence, embedding in zip(sentences, sentence_embeddings):
         print("Sentence:", sentence)
         print("Embedding:", embedding)
         print("")
+    TT.end_profile()
+    print(TT)
