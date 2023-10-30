fbd9b9e03687a24e5f880005b440e8aa7e69eef7
hassebp <49819884+hassebp@users.noreply.github.com>
2023-10-30 20:52:51 +0100

    sbert embeddings working

--- models.py
+++ models.py
@@ -1,1 +1,13 @@
-import numpy
+from sentence_transformers import SentenceTransformer
+
+def SBERT():
+    model = SentenceTransformer('all-MiniLM-L6-v2')
+
+    sentences = ['This framework generates embeddings for each input sentence',
+    'Sentences are passed as a list of string.', 
+    'The quick brown fox jumps over the lazy dog.']
+    sentence_embeddings = model.encode(sentences)
+    for sentence, embedding in zip(sentences, sentence_embeddings):
+        print("Sentence:", sentence)
+        print("Embedding:", embedding)
+        print("")
--- tester.py
+++ tester.py
@@ -1,5 +1,6 @@
 import torch
 import argparse
+from models import SBERT
 
 def check_cuda_availability():
     if torch.cuda.is_available() == True:
@@ -7,18 +8,21 @@ def check_cuda_availability():
     else: 
         return print('Cuda is not setup properly')
 
-
+def test_sbert():
+    SBERT()
 
 def run_tests():
     parser = argparse.ArgumentParser(description='Run a specific function in the script.')
     
     # Add functions to be tested in the choices list
-    parser.add_argument('function', choices=['check_cuda_availability'], help='Name of the function to run')
+    parser.add_argument('function', choices=['check_cuda_availability','test_sbert'], help='Name of the function to run')
     args = parser.parse_args()
 
     # Call the specified function
     if args.function == 'check_cuda_availability':
         check_cuda_availability()
+    elif args.function == 'test_sbert':
+        test_sbert()
     else:
         print(f"Error: Unknown function '{args.function}'")
 
